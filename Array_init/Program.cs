55df33fe0b1ef3a003fa7bba19f61f6cbe44de20
michelle <31318@ma-web.nl>
2021-10-03 10:11:31 +0200

    Finished array_init

---
+++ Array_init/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Array_init
+{
+    class Program
+    {
+        static void Main()
+        {
+            Room[,] newRoom = new Room[5, 6];
+        }
+    }
+
+    class Room
+    {
+        public Tile [,] myTileArray;
+
+        public Room (int width, int height)
+        {
+            myTileArray = new Tile [width, height];
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    myTileArray[i, j] = new Tile();
+                }
+            }
+        }
+
+    }
+
+
+    class Tile
+    {
+
+    }
+
+}
